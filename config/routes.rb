87dbc51fd583a6050c44b16c561d8ea2a59cc339
Kilomaster_3 <hromalex.2011@gmail.com>
2020-03-30 17:44:08 +0300

    Facebook and Linkedin Feath

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_scope :user do
+    get '/auth/facebook/callback' => 'users/omniauth_callbacks#facebook'
+    get '/auth/linkedin/callback' => 'users/omniauth_callbacks#linkedin'
+
+    get '/auth/linkedin' => 'users/omniauth_callbacks#linkedin'
+  end
+end
